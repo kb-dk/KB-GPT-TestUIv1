a298d15ee0a6cf8f1c47eee1bbe4ba9890bc2048
dhlakb <dhla@kb.dk>
2025-05-14 13:24:41 +0200

    Reverse last change

--- frontend/src/components/QuestionInput/QuestionInput.tsx
+++ frontend/src/components/QuestionInput/QuestionInput.tsx
@@ -22,7 +22,7 @@ export const QuestionInput = ({ onSend, disabled, placeholder, clearOnSend, conv
   const [base64Image, setBase64Image] = useState<string | null>(null);
 
   const appStateContext = useContext(AppStateContext)
-  const OYD_ENABLED = true;
+  const OYD_ENABLED = appStateContext?.state.frontendSettings?.oyd_enabled || false;
 
   const handleImageUpload = async (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
